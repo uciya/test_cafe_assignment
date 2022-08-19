e77b083e3336673a626af1f4b6325298cf76c811
unknown <umutciya15@gmail.com>
2022-08-19 07:29:46 +0300

    Capability of switching environment is enhanced

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -3,9 +3,9 @@ import config from '../config'
 
 module.exports = class BasePage {
 
-    async ensure_page(page_address){
+    async ensure_page(environment, page_address){
         let current_url = ClientFunction(() => window.location.href);
         await t
-            .expect(current_url()).contains(`${config.target_env[page_address]}`);
+            .expect(current_url()).contains(`${config[environment][page_address]}`);
     }
 }
--- tests/test_flow.js
+++ tests/test_flow.js
@@ -7,31 +7,32 @@ import CartPage from '../pages/CartPage';
 import CheckoutOnePage from '../pages/CheckoutOnePage';
 import CheckoutTwoPage from '../pages/CheckoutTwoPage';
 
+let environment = config.environment; //You can change environment by updating value of "environment" key of config.json
 
 fixture `Swag Labs Flow Automation`
-    .page `${config.target_env.baseUrl}`;
+    .page `${config[environment].baseUrl}`;
 
 test('Check total price of cart', async t => {
     let case_params = test_params.dataset_1;
     let item_count = case_params.item_count_to_add;
-    await LoginPage.login(`${config.target_env.valid_username}`, `${config.target_env.valid_password}`);
+    await LoginPage.login(`${config[environment].valid_username}`, `${config[environment].valid_password}`);
     
-    await InventoryPage.ensure_page("inventory_address");
+    await InventoryPage.ensure_page(environment, "inventory_address");
     await InventoryPage.add_item_to_cart(item_count);
     await InventoryPage.verify_item_count(item_count);
     //await t.wait(3000);
     await InventoryPage.go_to_cart();
 
-    await CartPage.ensure_page("cart_address");
+    await CartPage.ensure_page(environment, "cart_address");
     await CartPage.verify_item_count(item_count);
     await CartPage.go_to_checkoutOne();
 
-    await CheckoutOnePage.ensure_page("checkout_one_address");
+    await CheckoutOnePage.ensure_page(environment, "checkout_one_address");
     await CheckoutOnePage
         .fill_in_form(case_params.customer_first_name, case_params.customer_last_name, case_params.customer_zip_code);
     await CheckoutOnePage.go_to_checkoutTwo();
 
-    await CheckoutTwoPage.ensure_page("checkout_two_address");
+    await CheckoutTwoPage.ensure_page(environment, "checkout_two_address");
     await CheckoutTwoPage.verify_item_count(item_count);
     await CheckoutTwoPage.verify_total_price_wo_tax();
     //await t.wait(5000);
